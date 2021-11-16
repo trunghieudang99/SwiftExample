0d2d57075bd8d677dc35dbdafd597bfc46ec5b6d
dangtrunghieu1999 <hieudt230899@gmail.com>
2021-11-16 13:46:28 +0700

    - using chart

--- RxSwiftExample/RxSwiftExample/Modules/Resource/AppDelegate.swift
+++ RxSwiftExample/RxSwiftExample/Modules/Resource/AppDelegate.swift
@@ -16,32 +16,11 @@ import IQKeyboardManagerSwift
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        IQKeyboardManager.shared.enable = true
-        
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.rootViewController = UINavigationController(rootViewController: ViewController())
-
         return true
-    }
-}
-
-extension AppDelegate {
-    
-    func swichRootViewController(_ viewController: UIViewController,
-                                 animated: Bool,
-                                 duration: Double = 0.35,
-                                 animationOption: UIView.AnimationOptions = .transitionCrossDissolve,
-                                 completion: ((Bool) -> Void)? = nil)  {
-        guard let window = self.window else { return }
-        if animated {
-            UIView.transition(with: window, duration: duration, options: animationOption, animations: {
-                window.rootViewController = viewController
-            }, completion: completion)
-        } else {
-            window.rootViewController = viewController
-        }
     }
 }
