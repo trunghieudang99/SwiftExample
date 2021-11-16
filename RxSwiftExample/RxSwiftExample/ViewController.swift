1d0504c465ea081410030e8403fa2ab35b689143
dangtrunghieu1999 <hieudt230899@gmail.com>
2021-11-16 14:42:37 +0700

    - implement complete scroll view

--- RxSwiftExample/RxSwiftExample/ViewController.swift
+++ RxSwiftExample/RxSwiftExample/ViewController.swift
@@ -9,9 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView(frame: .zero)
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.backgroundColor = .white
+        return scrollView
+    }()
+
+    fileprivate lazy var rewardsImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.masksToBounds = true
+        imageView.image = UIImage(named: "dineIn-menu-1")
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .white
+        layoutScrollView()
+        layoutRewardsImageView()
     }
-
+    
+    private func layoutScrollView() {
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints { make in
+            if #available(iOS 11, *) {
+                make.top.equalTo(view.safeAreaInsets)
+            } else {
+                make.top.equalTo(topLayoutGuide.snp.bottom)
+            }
+            make.top.equalToSuperview()
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+    }
+    
+    private func layoutRewardsImageView() {
+        scrollView.addSubview(rewardsImageView)
+        rewardsImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.left.right.equalTo(view)
+            make.bottom.equalToSuperview()
+                .offset(-5)
+        }
+    }
+    
 }
